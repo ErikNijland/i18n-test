21a6920c1c42fcc2023000450d9ef60bd3c2a3b7
ErikNijland <e.n.nijland@gmail.com>
2018-06-27 22:42:02 +0200

    feat(locale): dynamic locale (needs refresh)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {LOCALE_ID, NgModule} from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {registerLocaleData} from '@angular/common';
+import localeFr from '@angular/common/locales/fr';
+import localeFrExtra from '@angular/common/locales/extra/fr';
+import localeNl from '@angular/common/locales/nl';
+import localeNlExtra from '@angular/common/locales/extra/nl';
+
+registerLocaleData(localeFr, 'fr', localeFrExtra);
+registerLocaleData(localeNl, 'nl', localeNlExtra);
 
 @NgModule({
   declarations: [
@@ -10,7 +18,21 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    // { provide: LOCALE_ID, useValue: 'nl-NL' },
+    { provide: LOCALE_ID, useFactory: getLocale },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+function getLocale() {
+  const supportedLocales = ['en-US', 'fr-FR', 'nl-NL'];
+  const defaultLocale = 'en-US';
+
+  const currentUrl = new URL(window.location.href);
+  const locale = currentUrl.searchParams.get('locale');
+  const isLocaleSupported = supportedLocales.includes(locale);
+
+  return isLocaleSupported ? locale : defaultLocale;
+}
